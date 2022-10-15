3bc3c984109325e8c2969566daadfd088700a335
marshall1632 <marshallmulanga@gmail.com>
2022-10-15 14:00:51 +0200

    upgrade

--- Assets/Scripts/Origami/OrigamiMovement.cs
+++ Assets/Scripts/Origami/OrigamiMovement.cs
@@ -6,21 +6,28 @@ using UnityEngine.InputSystem;
 public class OrigamiMovement : MonoBehaviour
 {
     [SerializeField]
-    private Camera Camera = null;
-    [SerializeField]
-    private LayerMask LayerMask;
-    [SerializeField]
-    private Vector3 CameraOffset;
+   // private Camera Camera = null;
+    //[SerializeField]
+    //private LayerMask LayerMask;
+    //[SerializeField]
+    [Range(0, 100)] public float speed;
+    [Range(0, 500)] public float radius;
+    //private Vector3 CameraOffset;
     private NavMeshAgent Agent;
 
     private void Awake()
     {
         Agent = GetComponent<NavMeshAgent>();
+        if (Agent != null)
+        {
+            Agent.speed = speed;
+            Agent.SetDestination(RandomNavMeshLoaction());
+        }
     }
 
     private void Update()
     {
-        if (Application.isFocused && Mouse.current.leftButton.wasReleasedThisFrame)
+        /**if (Application.isFocused && Mouse.current.leftButton.wasReleasedThisFrame)
         {
             Ray ray = Camera.ScreenPointToRay(Mouse.current.position.ReadValue());
 
@@ -28,11 +35,26 @@ public class OrigamiMovement : MonoBehaviour
             {
                 Agent.SetDestination(hit.point);
             }
+        }*/
+        if (Agent != null && Agent.remainingDistance <= Agent.stoppingDistance) 
+        {
+            Agent.SetDestination(RandomNavMeshLoaction());
         }
     }
 
-    private void LateUpdate()
+   /** private void LateUpdate()
     {
         Camera.transform.position = transform.position + CameraOffset;
+    }*/
+    public Vector3 RandomNavMeshLoaction()
+    {
+        Vector3 finalposition = Vector3.zero;
+        Vector3 randomposition = Random.insideUnitSphere * radius;
+        randomposition += transform.position;
+        if (NavMesh.SamplePosition(randomposition, out NavMeshHit hit, radius, 1))
+        {
+            finalposition = hit.position;
+        }
+        return finalposition;   
     }
 }
---
+++ Assets/Scripts/Origami/OrigamiShape.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class OrigamiShape : MonoBehaviour
+{
+    Mesh mymesh;
+    MeshFilter meshFilter;
+
+    [SerializeField] Vector2 planeSize = new Vector2(1, 1);
+    [SerializeField] int planeResolution = 1;
+
+    List<Vector3> vertices;
+    List<int> triangles;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mymesh = new Mesh();
+        meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = mymesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        planeResolution = Mathf.Clamp(planeResolution, 1, 50);
+
+        GeneratePlane(planeSize, planeResolution);
+        LeftToRightSine(Time.timeSinceLevelLoad);
+        AssignMesh();
+    }
+
+    void GeneratePlane(Vector2 size, int resolution)
+    {
+        // create vertices
+        vertices = new List<Vector3>();
+        float xPerStep = size.x / resolution;
+        float yPerStep = size.y / resolution;
+        for (int y = 0; y < resolution + 1; y++)
+        {
+            for (int x = 0; x < resolution + 1; x++)
+            {
+                vertices.Add(new Vector3(x * xPerStep, 0, y * yPerStep));
+            }
+        }
+
+        //create triangles
+        triangles = new List<int>();
+        for (int row = 0; row < resolution; row++)
+        {
+            for (int column = 0; column < resolution; column++)
+            {
+                int i = (row * resolution) + row + column;
+
+                //first triangle
+                triangles.Add(i);
+                triangles.Add(i + (resolution) + 1);
+                triangles.Add(i + (resolution) + 2);
+
+                //second triangle
+                triangles.Add(i);
+                triangles.Add(i + resolution + 2);
+                triangles.Add(i + 1);
+            }
+        }
+
+    }
+
+    void AssignMesh()
+    {
+        mymesh.Clear();
+        mymesh.vertices = vertices.ToArray();
+        mymesh.triangles = triangles.ToArray();
+    }
+
+    void LeftToRightSine(float time)
+    {
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            Vector3 vertex = vertices[i];
+            vertex.y = Mathf.Sin(time + vertex.x);
+            vertices[i] = vertex;
+        }
+    }
+}
